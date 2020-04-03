d2316d7bfce6115e5f53b9bc3d377c9fd8e63a5e
Eduard-Nicola <nicola.mihai.eduard@gmail.com>
2020-04-03 11:35:28 +0300

    Implemented tasks for Laboratory 06

--- labtasks/FTPServerWelcomeMessage/app/src/main/java/ro/pub/cs/systems/eim/lab06/ftpserverwelcomemessage/network/FTPServerCommunicationAsyncTask.java
+++ labtasks/FTPServerWelcomeMessage/app/src/main/java/ro/pub/cs/systems/eim/lab06/ftpserverwelcomemessage/network/FTPServerCommunicationAsyncTask.java
@@ -4,9 +4,11 @@ import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
 import java.net.Socket;
 
 import ro.pub.cs.systems.eim.lab06.ftpserverwelcomemessage.general.Constants;
+import ro.pub.cs.systems.eim.lab06.ftpserverwelcomemessage.general.Utilities;
 
 public class FTPServerCommunicationAsyncTask extends AsyncTask<String, String, Void> {
 
@@ -23,12 +25,24 @@ public class FTPServerCommunicationAsyncTask extends AsyncTask<String, String, V
             // TODO exercise 4
             // open socket with FTPServerAddress.getText().toString() (taken from param[0]) and port (Constants.FTP_PORT = 21)
             // get the BufferedReader attached to the socket (call to the Utilities.getReader() method)
-            // should the line start with Constants.FTP_MULTILINE_STARTCODE = "220-", the welcome message is processed
+            // should the line start with Constants.FTP_MULTILINE_START_CODE = "220-", the welcome message is processed
             // read lines from server while
             // - the value is different from Constants.FTP_MULTILINE_END_CODE1 = "220"
             // - the value does not start with Constants.FTP_MULTILINE_END_CODE2 = "220 "
             // append the line to the welcomeMessageTextView text view content (on the UI thread !!!) - publishProgress(...)
             // close the socket
+            socket = new Socket(params[0], Constants.FTP_PORT);
+            BufferedReader reader = Utilities.getReader(socket);
+
+            String line = reader.readLine();
+            if (line.substring(0, 4).equals(Constants.FTP_MULTILINE_START_CODE)) {
+                while (!line.equals(Constants.FTP_MULTILINE_END_CODE1) || !line.substring(0, 4).equals(Constants.FTP_MULTILINE_END_CODE2)) {
+                    publishProgress(line);
+                    line = reader.readLine();
+                }
+            }
+
+            socket.close();
         } catch (Exception exception) {
             Log.d(Constants.TAG, exception.getMessage());
             if (Constants.DEBUG) {
@@ -44,9 +58,10 @@ public class FTPServerCommunicationAsyncTask extends AsyncTask<String, String, V
     }
 
     @Override
-    protected void onProgressUpdate(String... progres) {
+    protected void onProgressUpdate(String... progress) {
         // TODO exercise 4
         // append the progress[0] to the welcomeMessageTextView text view
+        welcomeMessageTextView.setText(welcomeMessageTextView.getText() + progress[0] + "\n");
     }
 
     @Override
--- labtasks/SingleThreadedServer/app/src/main/java/ro/pub/cs/systems/eim/lab06/singlethreadedserver/network/ServerThread.java
+++ labtasks/SingleThreadedServer/app/src/main/java/ro/pub/cs/systems/eim/lab06/singlethreadedserver/network/ServerThread.java
@@ -18,6 +18,42 @@ public class ServerThread extends Thread {
     private ServerSocket serverSocket;
     private EditText serverTextEditText;
 
+    private class ConnectionHandler extends Thread {
+        Socket socket;
+
+        ConnectionHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Log.v(Constants.TAG, "Connection opened with " + socket.getInetAddress() + ":" + socket.getLocalPort());
+
+                // TODO exercise 5c
+                // simulate the fact the communication routine between the server and the client takes 3 seconds
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException interruptedException) {
+                    Log.e(Constants.TAG, interruptedException.getMessage());
+                    if (Constants.DEBUG) {
+                        interruptedException.printStackTrace();
+                    }
+                }
+
+                PrintWriter printWriter = Utilities.getWriter(socket);
+                printWriter.println(serverTextEditText.getText().toString());
+                socket.close();
+                Log.v(Constants.TAG, "Connection closed");
+            } catch (IOException ioException) {
+                Log.e(Constants.TAG, "An exception has occurred: " + ioException.getMessage());
+                if (Constants.DEBUG) {
+                    ioException.printStackTrace();
+                }
+            }
+        }
+    }
+
     public ServerThread(EditText serverEditText) {
         this.serverTextEditText = serverEditText;
     }
@@ -49,19 +85,11 @@ public class ServerThread extends Thread {
             serverSocket = new ServerSocket(Constants.SERVER_PORT);
             while (isRunning) {
                 Socket socket = serverSocket.accept();
-                Log.v(Constants.TAG, "Connection opened with " + socket.getInetAddress() + ":" + socket.getLocalPort());
-
-                // TODO exercise 5c
-                // simulate the fact the communication routine between the server and the client takes 3 seconds
-
-                PrintWriter printWriter = Utilities.getWriter(socket);
-                printWriter.println(serverTextEditText.getText().toString());
-                socket.close();
-                Log.v(Constants.TAG, "Connection closed");
 
                 // TODO exercise 5d
                 // move the communication routine between the server and the client on a separate thread (each)
-
+                ConnectionHandler connectionHandler = new ConnectionHandler(socket);
+                connectionHandler.start();
             }
         } catch (IOException ioException) {
             Log.e(Constants.TAG, "An exception has occurred: " + ioException.getMessage());
@@ -69,5 +97,6 @@ public class ServerThread extends Thread {
                 ioException.printStackTrace();
             }
         }
+
     }
 }
